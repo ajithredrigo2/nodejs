d20877b3802d56c01e146d10b0a02de52c97feb6
root <root@DevOps.0igbhsr1yheezou5z4opsxtrtd.bx.internal.cloudapp.net>
2023-09-02 06:28:53 +0000

    add

--- routes/index.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Controllers
-const { renderIndex, renderAbout } = require("../controllers/index.controller");
-
-router.get("/", renderIndex);
-module.exports = router;
